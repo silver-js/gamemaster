bf532bab0fcfd955b2221fed284b7cd8ae1191aa
silver <silver.js.dev@gmail.com>
2025-03-13 23:58:19 -0300

    minor fixes, fixing compilation error handling, adding position fixed to body

--- dev/gmEditor/romPlayer.js
+++ dev/gmEditor/romPlayer.js
@@ -3,88 +3,66 @@ let playState = false;
 const domPlayStop = document.getElementById('play-stop-btn');
 const domEditor = document.getElementById('editor-wrapper');
 const domGame = document.getElementById('game-wrapper');
-
-const playPause = ()=>{
-  playState = !playState;
-  domPlayStop.innerHTML = playState?'&#x23f9 Stop':'&#9658 Play';
-  domEditor.style.display = playState?'none':'flex';
-  domGame.style.display = playState?'flex':'none';
-  if(playState){
-    playRom();
-    return
-  }
-  stopRom()
-}
-domPlayStop.addEventListener('click',playPause);
-
 const gameWindow = document.getElementById('game-iframe');
-const headHtml = `
-  <head>
-    <style>
-      *{
-        color:pink;
-      }
-    </style>
-  </head>
-`;
-const bodyHtml = `
-  <body>
-    <div id="game-wrapper"></div>
-  </body>
-`;
-const preScript = `
-<script type="module">
-  import {_loop, _pad, _cfg} from './modules/gmCore_min.js';
-  import gfx from './modules/gfx.js';
-  
-  for(let k in console){
-    console[k] = parent.console[k];
-  }
+
+const integrityCheck = (code)=>{
   try{
-`;
-const endScript = `
+    Function(code);
+    return true;
   }
-  catch(_e){
-    console.log(_e);
+  catch(e){
+    console.log('COMPILATION ERROR:', e);
+    const codeArr = code.split('\n');
+    let lineError = 0;
+    for(let i = codeArr.length; i >= 0; i--){
+      try{
+        Function(codeArr.slice(0, i).join('\n'));
+        lineError = i + 1;
+        i= -1;
+      }
+      catch(e2){}
+    }
+    alert(`COMPILATION ERROR on line ${codeLineOffset + lineError}:\n ${codeArr[lineError - 1]}`);
+    return false;
+  }
 }
-</script>
-`;
-export const playRom = ()=>{
+const preCode = `<head><style>*{color:pink;}</style></head><body><div id="game-wrapper"></div></body>`;
+const preScripts = `<script type="module">import {_loop, _pad, _cfg} from './modules/gmCore_min.js';import gfx from './modules/gfx.js';for(let k in console){console[k] = parent.console[k];}try{`;
+const endScript = `}catch(_e){console.log(_e);}</script>`;
+
+const playRom = (code)=>{
+  /*
   let atlasCode = 'let _al;'
   const atlas = db.rom.atlas;
   for(let k in atlas){
     atlasCode += `let ${k} = [];_al = async ()=>{let x = await gfx.loadAtlas('${atlas[k].src}', ${atlas[k].tileSize}, ${atlas[k].scale}, ${atlas[k].offsetX}, ${atlas[k].offsetY}, ${atlas[k].hMargin}, ${atlas[k].vMargin});${k} = x};_al();`;
   }
   atlasCode += `_al=null;`;
-  const romCode = db.rom.code.join('\n');
   try{
-    console.clear();
-    Function(romCode);
     gameWindow.srcdoc = headHtml + bodyHtml + preScript + atlasCode + romCode + endScript
   }
   catch(_e){
-    const strArr = romCode.split('\n');
-    let lastLine = "";
-    
-    for(let i = strArr.length; i > 0; i--){
-      lastLine = strArr.pop();
-      try{
-        Function(strArr.join('\n'));
-        for(let j = lastLine.length - 1; j > -1; j--){
-          try{
-            Function(strArr.join('\n') + lastLine.slice(0,j));
-            console.error(`ROM compilation Error, line ${i + lineOffset}, position ${j}.\n`,_e);
-            j = -1;
-          }
-          catch(_errj){}
-        }
-        i = 0;
-      }
-      catch(_erri){}
-    }
   }
-
+  */
+  gameWindow.srcdoc = preCode + preScripts + code + endScript;
 }
-export const stopRom = ()=>{
+const stopRom = ()=>{
   gameWindow.srcdoc = '';
 }
+const playPause = ()=>{
+  playState = !playState;
+  domPlayStop.innerHTML = playState?'&#x23f9 Stop':'&#9658 Play';
+  domEditor.style.display = playState?'none':'flex';
+  domGame.style.display = playState?'flex':'none';
+  const romCode = db.rom.code.join('\n');
+  if(playState){
+    console.clear();
+    if(integrityCheck(romCode)){
+      playRom(romCode);
+    }
+    return
+  }
+  stopRom()
+}
+domPlayStop.addEventListener('click',playPause);
+
