ed75f11ba0ead7dd605ce93caf9327b1f9ef98eb
silver <silver.js.dev@gmail.com>
2025-08-11 22:19:31 -0300

    update tools

--- dev/tools/main.js
+++ dev/tools/main.js
@@ -3,10 +3,13 @@ import cpu from './modules/cpu_min.js';
 
 // img to data
 const imgToDataInput = document.getElementById("img-to-data-input");
+const imgDataText = document.getElementById("img-to-data-text");
+
 imgToDataInput.addEventListener('change', (e)=>{
 	const reader = new FileReader();
 	reader.addEventListener('load', ()=>{
 		console.log(reader.result);
+		imgDataText.innerHTML = reader.result
 	}, false);
 	reader.readAsDataURL(e.target.files[0]);
 });
